c3ee979e06ecdd892e444e2a61048fa4c2c5a3ce
Mikhail Zhelezin <mihonblack@gmail.com>
2019-11-25 18:53:46 +0300

    Runtime: 2.93, diff = 0.0014

--- task.cpp
+++ task.cpp
@@ -1,42 +1,52 @@
 #include "task.h"
 #include<bits/stdc++.h>
-
+#include "omp.h"
 using namespace std;
 
 void performQueries(int32_t nRows, int32_t nCols, int32_t nQueries, int32_t nRes, double* data, int32_t* queries, double* result)
 {
     // Reference unoptimized code, replace it with your solution
     std::vector<bool> used(nQueries, 0);
-    auto check = [&](int queryId){
+    auto check = [&](int queryId, vector<double> &current_result){
         if(used[queryId]) return;
         used[queryId] = 1;
         int32_t diff = queryId << 2;
-        int32_t rowA = queries[diff];
-        int32_t colA = queries[diff + 1];
-        int32_t rowB = queries[diff + 2];
-        int32_t colB = queries[diff + 3];
-        for(int32_t col = 0; col < nRes; ++col)
+        short rowA = queries[diff];
+        short colA = queries[diff + 1];
+        short rowB = queries[diff + 2];
+        short colB = queries[diff + 3];
+        for(short col = 0; col < nRes; ++col)
         {
-            for (int32_t row = 0; row < nRes; ++row)
+            for (short row = 0; row < nRes; ++row)
             {
-                int32_t cRowA = rowA + row;
-                int32_t cColA = colA + col;
-                int32_t cRowB = rowB + row;
-                int32_t cColB = colB + col;
-                result[row * nRes + col] += data[cRowA * nCols + cColA] * data[cRowB * nCols + cColB];
+                short cRowA = rowA + row;
+                short cColA = colA + col;
+                short cRowB = rowB + row;
+                short cColB = colB + col;
+                current_result[row * nRes + col] += data[cRowA * nCols + cColA] * data[cRowB * nCols + cColB];
             }
         }
         return;
     };
     int query = 0;
-    #pragma omp parallel
+    omp_set_num_threads(omp_get_max_threads());
+    
+    #pragma omp parallel 
     {
-        #pragma for shared(result) private(queries, data, query)  reduction(+:result) schedule(guided)
-        for(query = 0; query < nQueries; query++)
-            check(query);
+        vector<double> current_result(nRes * nRes, 0);
+        #pragma for private(query) schedule(static)
+        for(query = 0; query < nQueries; ++query)
+            check(query, current_result);
+            
+        for (short i = 0; i < nRes; i++){
+            for(short j = 0; j < nRes; j++){
+                short ind = i * nRes + j;
+                #pragma omp atomic
+                result[ind] += current_result[ind];
+            }
+        }
     }
 
     #pragma omp barrier
-   
     // Reference unoptimized code, replace it with your solution
 }
