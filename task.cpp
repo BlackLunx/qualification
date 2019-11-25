cbaf6f84e1297c4e95e25cd1ba0701cd278094c0
Mikhail Zhelezin <mihonblack@gmail.com>
2019-11-25 17:37:11 +0300

    runtime 100k: 4s  diff 100k: 0.007

--- task.cpp
+++ task.cpp
@@ -1,29 +1,42 @@
 #include "task.h"
+#include<bits/stdc++.h>
+
+using namespace std;
 
 void performQueries(int32_t nRows, int32_t nCols, int32_t nQueries, int32_t nRes, double* data, int32_t* queries, double* result)
 {
     // Reference unoptimized code, replace it with your solution
-
-    for (int32_t queryId = 0; queryId < nQueries; queryId++)
-    {
-        int32_t rowA = queries[queryId * 4 + 0];
-        int32_t colA = queries[queryId * 4 + 1];
-        int32_t rowB = queries[queryId * 4 + 2];
-        int32_t colB = queries[queryId * 4 + 3];
-
-        for (int32_t col = 0; col < nRes; col++)
+    std::vector<bool> used(nQueries, 0);
+    auto check = [&](int queryId){
+        if(used[queryId]) return;
+        used[queryId] = 1;
+        int32_t diff = queryId << 2;
+        int32_t rowA = queries[diff];
+        int32_t colA = queries[diff + 1];
+        int32_t rowB = queries[diff + 2];
+        int32_t colB = queries[diff + 3];
+        for(int32_t col = 0; col < nRes; ++col)
         {
-            for (int32_t row = 0; row < nRes; row++)
+            for (int32_t row = 0; row < nRes; ++row)
             {
                 int32_t cRowA = rowA + row;
                 int32_t cColA = colA + col;
                 int32_t cRowB = rowB + row;
                 int32_t cColB = colB + col;
-
                 result[row * nRes + col] += data[cRowA * nCols + cColA] * data[cRowB * nCols + cColB];
             }
         }
+        return;
+    };
+    int query = 0;
+    #pragma omp parallel
+    {
+        #pragma for shared(result) private(queries, data, query)  reduction(+:result) schedule(guided)
+        for(query = 0; query < nQueries; query++)
+            check(query);
     }
 
+    #pragma omp barrier
+   
     // Reference unoptimized code, replace it with your solution
 }
