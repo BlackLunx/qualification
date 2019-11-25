0c1ba6c598bd108dc61fd52764cf9ac1bc8b55d8
OnlyDeniko <kulandin08@gmail.com>
2019-11-25 23:36:56 +0300

    final shit

--- task.cpp
+++ task.cpp
@@ -6,10 +6,14 @@ using namespace std;
 void performQueries(int32_t nRows, int32_t nCols, int32_t nQueries, int32_t nRes, double* data, int32_t* queries, double* result)
 {
     // Reference unoptimized code, replace it with your solution
-    std::vector<bool> used(nQueries, 0);
-    auto check = [&](int queryId, vector<double> &current_result){
+    bool* used = new bool[nQueries];
+    memset(used, 0, nQueries);
+//      vector<bool> used(nQueries, 0);
+//    const int sz = 20000000;
+//    bitset<sz> used(0);
+    auto check = [&](int queryId, double* current_result){
         if(used[queryId]) return;
-        used[queryId] = 1;
+        used[queryId] = true;
         int32_t diff = queryId << 2;
         short rowA = queries[diff];
         short colA = queries[diff + 1];
@@ -28,18 +32,20 @@ void performQueries(int32_t nRows, int32_t nCols, int32_t nQueries, int32_t nRes
     };
     omp_set_num_threads(omp_get_max_threads());
     short len = nRes * nRes;
-    
-    #pragma omp parallel 
+    #pragma omp parallel
     {
-        vector<double> current_result(len, 0);
+        double* current_result = new double[len];
+        memset(current_result, 0, len);
+//        vector<double> current_result(len, 0);
         #pragma omp for schedule(static)
         for(int query = 0; query < nQueries; ++query)
             check(query, current_result);
-        
+
         for (short ind = 0; ind < len; ++ind){
             #pragma omp atomic
             result[ind] += current_result[ind];
         }
+        delete[] current_result;
     }
 
     #pragma omp barrier
